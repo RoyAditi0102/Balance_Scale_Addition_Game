f51ab59387a4c34edb1fa54414b64ca5d6cc1212
Aditi Roy <royaditi0102@gmail.com>
2025-03-12 16:36:59 +0530

    Redeploying frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
 export default defineConfig({
   define: {
-    "process.env.VITE_API_URL": "https://balance-scale-addition-game.onrender.com",
+    "process.env.VITE_API_URL": JSON.stringify("https://balance-scale-addition-game.onrender.com"),
   },
-});
-
+  plugins: [vue()],
+})
