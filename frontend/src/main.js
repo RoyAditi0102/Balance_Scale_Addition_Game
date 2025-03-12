10ab8c234bcf72f978c98ba0c1ffd6f66acb2ea2
Aditi Roy <royaditi0102@gmail.com>
2025-03-12 15:56:48 +0530

    Redeploying frontend and backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,6 @@ import './assets/style.css'
 import App from './App.vue'
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
+fetch(`${API_BASE_URL}/game-state`)
+
 createApp(App).mount('#app')
